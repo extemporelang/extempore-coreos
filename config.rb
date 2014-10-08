9aa84af666c253612884259bb12f6d3208878948
Ben Swift <benjamin.j.swift@gmail.com>
2014-10-08 11:50:52 +1100

    expose docker tcp port (needed for fleetctl)

--- config.rb
+++ config.rb
@@ -43,7 +43,7 @@ $update_channel='alpha'
 # If 2375 is used, Vagrant will auto-increment (e.g. in the case of $num_instances > 1)
 # You can then use the docker tool locally by setting the following env var:
 #   export DOCKER_HOST='tcp://127.0.0.1:2375'
-#$expose_docker_tcp=2375
+$expose_docker_tcp=2375
 
 # Setting for VirtualBox VMs
 #$vb_gui = false
