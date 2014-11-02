299b9b1101e83a0c567d050ac4b2731752568a5c
Ben Swift <benjamin.j.swift@gmail.com>
2014-11-02 14:45:21 +1100

    extempore-logview.sh improvements

--- extempore-logview.sh
+++ extempore-logview.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-: ${EXTEMPORE_HOSTS:="172.17.8.101 172.17.8.102"}
-CURL_PIDS=""
-
-for host in $EXTEMPORE_HOSTS
-do
-    curl http://$host:8000/logs & CURL_PIDS="$! $CURL_PIDS"
-done
-
-echo "kill this shell, or run kill $CURL_PIDS to kill the curl processes"
+echo http://{172.17.8.101,172.17.8.102,172.17.8.103}:8000/logs | xargs -P 10 -n 1 curl
 
 exit 0
